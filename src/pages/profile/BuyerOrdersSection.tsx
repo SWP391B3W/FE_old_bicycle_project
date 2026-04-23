29c05ace223e258511fae0032bd7ca5b4352a3d7
tandat1408 <tandatofficial3@gmail.com>
2026-04-23 22:28:36 +0700

    fix: import missing canBuyerRequestRefund helper to fix runtime error

--- src/pages/profile/BuyerOrdersSection.tsx
+++ src/pages/profile/BuyerOrdersSection.tsx
@@ -14,6 +14,7 @@ import { Flag } from 'lucide-react'
 import {
   canBuyerConfirmReceived,
   canBuyerRequestPayment,
+  canBuyerRequestRefund,
   canBuyerSubmitReview,
   canCancelOpenOrder,
   formatOrderCurrency,
